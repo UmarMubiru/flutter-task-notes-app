1e64dfa4b7fa473cb3580016743a825815887419
UmarMubiru <umarmubiru330@gmail.com>
2025-11-14 15:34:32 +0300

    integrated sqflite database helper

---
+++ lib/helpers/database_helper.dart
@@ -0,0 +1,91 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import '../models/task_item.dart';
+
+class DatabaseHelper {
+  static final DatabaseHelper instance = DatabaseHelper._init();
+  static Database? _database;
+
+  DatabaseHelper._init();
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDB('tasks.db');
+    return _database!;
+  }
+
+  Future<Database> _initDB(String filePath) async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, filePath);
+
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: _createDB,
+    );
+  }
+
+  Future<void> _createDB(Database db, int version) async {
+    const idType = 'INTEGER PRIMARY KEY AUTOINCREMENT';
+    const textType = 'TEXT NOT NULL';
+    const intType = 'INTEGER NOT NULL';
+
+    await db.execute('''
+      CREATE TABLE tasks (
+        id $idType,
+        title $textType,
+        priority $textType,
+        description $textType,
+        isCompleted $intType
+      )
+    ''');
+  }
+
+  // Insert a new TaskItem
+  Future<int> insertTask(TaskItem task) async {
+    final db = await database;
+    return await db.insert(
+      'tasks',
+      task.toJson(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  // Retrieve all TaskItems
+  Future<List<TaskItem>> getAllTasks() async {
+    final db = await database;
+    final List<Map<String, dynamic>> maps = await db.query('tasks');
+
+    return List.generate(maps.length, (i) {
+      return TaskItem.fromJson(maps[i]);
+    });
+  }
+
+  // Delete a TaskItem by id
+  Future<int> deleteTask(int id) async {
+    final db = await database;
+    return await db.delete(
+      'tasks',
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+  }
+
+  // Update a TaskItem
+  Future<int> updateTask(TaskItem task) async {
+    final db = await database;
+    return await db.update(
+      'tasks',
+      task.toJson(),
+      where: 'id = ?',
+      whereArgs: [task.id],
+    );
+  }
+
+  // Close the database
+  Future<void> close() async {
+    final db = await database;
+    db.close();
+  }
+}
+
